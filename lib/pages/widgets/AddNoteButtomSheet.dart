2be888c8cabe7f7a0cbeae364c5b241c27565933
mohand <mohanadalnjjar23@gmail.com>
2023-10-29 10:58:27 +0200

    adding field to sheet butttom

--- lib/pages/widgets/AddNoteButtomSheet.dart
+++ lib/pages/widgets/AddNoteButtomSheet.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/pages/widgets/CutomeTextField.dart';
 
 class AddNoteButtomSheet extends StatelessWidget {
   const AddNoteButtomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Column(
+        children:  [
+          SizedBox(
+            height: 40,
+          ),
+          CutomeTextField(
+            hintText: 'Topic',
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          CutomeTextField(
+            hintText: 'Content',
+            MaxLines: 8,
+          ),
+
+          
+        ],
+      ),
+    );
   }
 }
---
+++ lib/pages/widgets/CutomeTextField.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/pages/widgets/constance.dart';
+
+class CutomeTextField extends StatelessWidget {
+  CutomeTextField({super.key, required this.hintText, this.MaxLines = 1});
+  final String hintText;
+  final int MaxLines;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      maxLines: MaxLines,
+      cursorColor: KprimaryColor,
+      decoration: InputDecoration(
+        hintText: hintText,
+        // hintStyle: TextStyle(color: KprimaryColor),
+        border: buildBorder(color: Colors.white),
+        enabledBorder: buildBorder(color: KprimaryColor),
+        focusedBorder: buildBorder(color: KprimaryColor),
+      ),
+    );
+  }
+
+  OutlineInputBorder buildBorder({color}) {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(10),
+      borderSide: BorderSide(color: color ?? Colors.white),
+    );
+  }
+}
