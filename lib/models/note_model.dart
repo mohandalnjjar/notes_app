41407a2693a525f2e6eccb363d974e6da314f3f7
mohand <mohanadalnjjar23@gmail.com>
2023-10-29 21:22:13 +0200

    crste note model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,12 @@
+class NoteModel {
+  final String title;
+  final String content;
+  final String data;
+  final int color;
+
+  NoteModel(
+      {required this.title,
+      required this.content,
+      required this.data,
+      required this.color});
+}
