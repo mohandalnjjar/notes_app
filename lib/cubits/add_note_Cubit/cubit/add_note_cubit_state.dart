621bf5c318d94c3cc608edca42c013150b035d1a
mohand <mohanadalnjjar23@gmail.com>
2023-11-04 07:11:06 +0200

    create note cubit & note states

---
+++ lib/cubits/add_note_Cubit/cubit/add_note_cubit_state.dart
@@ -0,0 +1,12 @@
+part of 'add_note_cubit_cubit.dart';
+
+@immutable
+sealed class AddNoteCubitState {}
+
+final class AddNoteCubitInitial extends AddNoteCubitState {}
+
+final class AddNoteLoading extends AddNoteCubitState {}
+
+final class AddNoteSuccess extends AddNoteCubitState {}
+
+final class AddNoteFaliur extends AddNoteCubitState {}
